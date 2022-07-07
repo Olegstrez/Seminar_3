1c1f91a4269805e6c35956666f9140a8d60d505d
Алла Седорина <kornilov.s@evrogas.local>
2022-07-07 17:10:32 +0300

    Добавил задачу 8

---
+++ Seminar_8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int a=5,b=8,i=1;
+Console.WriteLine("Количество четных чисел 5");
+while (i<(a+1))
+{
+    if(i%2 ==0)
+    {
+Console.WriteLine("n=" + i);
+    }
+    
+    i++;
+}
+i=1;
+Console.WriteLine("Количество четных чисел 8");
+while (i<(b+1))
+{
+    if(i%2 ==0)
+    {
+Console.WriteLine("n=" + i);
+    }
+    
+    i++;
+}
+
