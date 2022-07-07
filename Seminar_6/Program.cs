a73a71ddd30192bd6564eeef47e4f78c9cdbaafe
Алла Седорина <kornilov.s@evrogas.local>
2022-07-07 16:36:41 +0300

    Добавил новые задачи 6 и 4

---
+++ Seminar_6/Program.cs
@@ -0,0 +1,36 @@
+﻿
+        Console.Clear () ;
+        double a1=4, b1=3, c1 = 7 ;
+        
+
+        Console.WriteLine("Проверка на четность а1 ");
+
+        if (a1 % 2 == 1)
+        {
+            Console.WriteLine("Нет");
+        }
+        else
+        {
+ Console.WriteLine("да");
+        }
+       
+Console.WriteLine("Проверка на четность b1 ");
+      
+if (b1 % 2 == 1)
+        {
+            Console.WriteLine("Нет");
+        }
+           else
+        {
+ Console.WriteLine("да");
+        }
+        Console.WriteLine("Проверка на четность c1 ");
+      
+if (c1 % 2 == 1)
+        {
+            Console.WriteLine("Нет");
+        }
+           else
+        {
+ Console.WriteLine("да");
+        }
